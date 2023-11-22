e953f7d64104b6fbab40a7c987049c3bd1450f69
Damian <contact@damians.space>
2023-11-22 20:46:47 +0100

    Initial

---
+++ File.cs
@@ -0,0 +1,207 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Wavestorm.Utilities;
+
+public partial class Utilities
+{
+    public class Files
+    {
+        /// <summary>
+        /// Reads a file from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The contents of the file.</returns>
+        public static string? ReadFile(string path)
+        {
+            return System.IO.File.ReadAllText(path);
+        }
+
+        /// <summary>
+        /// Reads a binary file from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The contents of the file.</returns>
+        public static byte[] ReadBinaryFile(string path)
+        {
+            return System.IO.File.ReadAllBytes(path);
+        }
+        
+        /// <summary>
+        /// Writes a file to the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file. If null, the current directory is used.</param>
+        /// <param name="contents">The contents of the file.</param>
+        /// <returns>True if the file was written successfully, false otherwise.</returns>
+        public static bool WriteFile([Optional] string? path, string contents)
+        {
+            path ??= System.AppDomain.CurrentDomain.BaseDirectory;
+
+            try
+            {
+                System.IO.File.WriteAllText(path, contents);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Writes a binary file to the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file. If null, the current directory is used.</param>
+        /// <param name="contents">The contents of the file.</param>
+        /// <returns>True if the file was written successfully, false otherwise.</returns>
+        public static bool WriteBinaryFile([Optional] string? path, byte[] contents)
+        {
+            path ??= System.AppDomain.CurrentDomain.BaseDirectory;
+
+            try
+            {
+                System.IO.File.WriteAllBytes(path, contents);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        
+        /// <summary>
+        /// Copies a file from the source path to the destination path.
+        /// </summary>
+        /// <param name="source">The source path.</param>
+        /// <param name="destination">The destination path.</param>
+        /// <returns>True if the file was copied successfully, false otherwise.</returns>
+        public static bool CopyFile(string source, string destination)
+        {
+            // TODO: Implement a better and more robust method of copying files.
+            try
+            {
+                System.IO.File.Copy(source, destination);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        
+        /// <summary>
+        /// Moves a file from the source path to the destination path.
+        /// </summary>
+        /// <param name="source">The source path.</param>
+        /// <param name="destination">The destination path.</param>
+        /// <returns>True if the file was moved successfully, false otherwise.</returns>
+        public static bool MoveFile(string source, string destination)
+        {
+            // TODO: Implement a better and more robust method of moving files.
+            try
+            {
+                System.IO.File.Move(source, destination);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Moves a directory from the source path to the destination path.
+        /// </summary>
+        /// <param name="source">The source path.</param>
+        /// <param name="destination">The destination path.</param>
+        /// <returns>True if the directory was moved successfully, false otherwise.</returns>
+        public static bool MoveDirectory(string source, string destination)
+        {
+            // TODO: Implement a better and more robust method of moving directories.
+            try
+            {
+                System.IO.Directory.Move(source, destination);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Deletes a file from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>True if the file was deleted successfully, false otherwise.</returns>
+        public static bool DeleteFile(string path)
+        {
+            try
+            {
+                System.IO.File.Delete(path);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Deletes a directory from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the directory.</param>
+        /// <returns>True if the directory was deleted successfully, false otherwise.</returns>
+        public static bool DeleteDirectory(string path)
+        {
+            try
+            {
+                System.IO.Directory.Delete(path);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the size of a file from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The size of the file in bytes.</returns>
+        public static long GetFileSize(string path)
+        {
+            return new System.IO.FileInfo(path).Length;
+        }
+
+        /// <summary>
+        /// Gets the creation time of a file from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The creation time of the file.</returns>
+        public static DateTime GetFileCreationTime(string path)
+        {
+            return new System.IO.FileInfo(path).CreationTime;
+        }
+
+        /// <summary>
+        /// Gets the last access time of a file from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The last access time of the file.</returns>
+        public static DateTime GetFileLastAccessTime(string path)
+        {
+            return new System.IO.FileInfo(path).LastAccessTime;
+        }
+
+        /// <summary>
+        /// Gets the last write time of a file from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The last write time of the file.</returns>
+        public static DateTime GetFileLastWriteTime(string path)
+        {
+            return new System.IO.FileInfo(path).LastWriteTime;
+        }
+    }
+}
---
+++ Network.cs
@@ -0,0 +1,116 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Wavestorm.Utilities;
+
+public partial class Utilities
+{
+    public class Network
+    {
+        /// <summary>
+        /// Checks if the device is connected to the internet.
+        /// </summary>
+        /// <returns>True if the device is connected to the internet, false otherwise.</returns>
+        public static bool IsConnectedToInternet()
+        {
+            return NetworkInterface.GetIsNetworkAvailable() && PingHost("google.com");
+        }
+
+        /// <summary>
+        /// Ping a host to check if it is reachable.
+        /// </summary>
+        /// <param name="host">The host to ping.</param>
+        /// <returns>True if the host is reachable, false otherwise.</returns>
+        public static bool PingHost(string host)
+        {
+            try
+            {
+                Ping ping = new();
+                var reply = ping.Send(host);
+                return reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Check if an URL has an secure connection.
+        /// </summary>
+        /// <param name="url">The URL to check.</param>
+        /// <returns>True if the URL has a secure connection, false otherwise.</returns>
+        public static bool IsSecureConnection(string url)
+        {
+            try
+            {
+                var uri = new Uri(url);
+                return uri.Scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase);
+            }
+            catch (UriFormatException)
+            {
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Get the public IP address of the device.
+        /// </summary>
+        /// <returns>The public IP address of the device.</returns>
+        public static async Task<string> GetPublicIpAddress()
+        {
+            if (IsConnectedToInternet())
+            {
+                const string url = "https://api.ipify.org";
+
+                using var httpClient = new HttpClient();
+                try
+                {
+                    var response = await httpClient.GetStringAsync(url);
+                    return response;
+                }
+                catch (HttpRequestException ex)
+                {
+                    return $"Error fetching public IP address: {ex.Message}";
+                }
+            }
+            else
+            {
+                return "No internet connection. Cannot get public IP address.";
+            }
+        }
+
+        /// <summary>
+        /// Check if a port is open.
+        /// </summary>
+        /// <param name="port">The port to check.</param>
+        /// <returns>True if the port is open, false otherwise.</returns>
+        public static bool IsPortOpen(int port)
+        {
+            var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+            foreach (var tcpi in tcpConnInfoArray)
+            {
+                if (tcpi.LocalEndPoint.Port == port)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// This method has not been implemented yet.
+        /// </summary>
+        public static void DownloadFile(string url, string path)
+        {
+            // TODO: Implement
+        }
+    }
+}
