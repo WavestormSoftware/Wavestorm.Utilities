95b14b55cad221dc46574d0e4acc1c8c6931a215
Damian <contact@damians.space>
2023-11-23 01:37:42 +0100

    Refactor Wavestorm Studio class to include GetVersion method for retrieving development version.

--- Wavestorm/Studio.cs
+++ Wavestorm/Studio.cs
@@ -1,6 +1,32 @@
 ﻿namespace Wavestorm.Utilities.Wavestorm;
 
-public class Studio
+public partial class Wavestorm
 {
-    
+    public class Studio
+    {
+        /// <summary>
+        /// Get the current development version of Wavestorm Studio.
+        /// </summary>
+        /// <returns>The current development version of Wavestorm Engine.</returns>
+        public static Version GetVersion()
+        {
+            if (Utilities.Network.IsConnectedToInternet())
+            {
+                var version = Utilities.Network.GetPlain("https://wavestormgames.net/api/studio/version/version.php").ToString();
+
+                if (version == null)
+                {
+                    return new Version(0, 0, 0, 0);
+                }
+                else
+                {
+                    return new Version(version);
+                }
+            }
+            else
+            {
+                return new Version(0, 0, 0, 0);
+            }
+        }
+    }
 }
