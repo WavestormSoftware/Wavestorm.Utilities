12a8806c1818b4fc71c4c1f22a6cfdfd69c21ba4
Damian <contact@damians.space>
2023-11-23 19:03:01 +0100

    Add new class "OS" to "SystemInfo" namespace with various methods to get operating system information.

---
+++ SystemInfo/OS.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Wavestorm.Utilities;
+
+public abstract partial class Utilities
+{
+    public partial class SystemInfo
+    {
+        public static class OS
+        {
+            /// <summary>
+            /// Gets the name of the operating system running on this computer.
+            /// </summary>
+            /// <returns>The name of the operating system running on this computer.</returns>
+            public static string GetOperatingSystemName()
+            {
+                return RuntimeInformation.OSDescription;
+            }
+
+            /// <summary>
+            /// Gets the version of the operating system running on this computer.
+            /// </summary>
+            /// <returns>The version of the operating system running on this computer.</returns>
+            public static string GetOperatingSystemVersion()
+            {
+                return Environment.OSVersion.VersionString;
+            }
+
+            /// <summary>
+            /// Gets the architecture of the operating system running on this computer.
+            /// </summary>
+            /// <returns>The architecture of the operating system running on this computer.</returns>
+            public static string GetOperatingSystemArchitecture()
+            {
+                return RuntimeInformation.OSArchitecture.ToString();
+            }
+
+            /// <summary>
+            /// Gets the name of the user who is currently logged in.
+            /// </summary>
+            /// <returns>The name of the user who is currently logged in.</returns>
+            public static string GetUserName()
+            {
+                return Environment.UserName;
+            }
+
+            /// <summary>
+            /// Gets the name of the computer.
+            /// </summary>
+            /// <returns>The name of the computer.</returns>
+            public static string GetComputerName()
+            {
+                return Environment.MachineName;
+            }
+
+            /// <summary>
+            /// Gets the user's language.
+            /// </summary>
+            /// <returns>The user's language.</returns>
+            public static string GetUserLanguage()
+            {
+                return CultureInfo.CurrentCulture.DisplayName;
+            }
+        }
+    }
+}
