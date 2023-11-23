e2be0789bca9e46210f566a6103b75b75b7f57ab
Damian <contact@damians.space>
2023-11-23 19:03:50 +0100

    Create new file `Tools.cs` in the `Network` namespace with a method `GetBaseDomain` that retrieves the base domain name from a full domain name.

---
+++ Network/Tools.cs
@@ -0,0 +1,30 @@
+﻿namespace Wavestorm.Utilities;
+
+public abstract partial class Utilities
+{
+    public partial class Network
+    {
+        public static class Tools
+        {
+            /// <summary>
+            /// Retrieves a base domain name from a full domain name. 
+            /// </summary>
+            /// <param name="domainName">Dns Domain name as a string</param>
+            /// <returns>Base domain name</returns>
+            public static string GetBaseDomain(string domainName)
+            {
+                var vars = domainName.Split('.');
+                if (vars == null || vars.Length != 3)
+                {
+                    return domainName;
+                }
+
+                var dom = new List<string>(vars);
+                var remove = vars.Length - 2;
+                dom.RemoveRange(0, remove);
+                return dom[0] + "." + dom[1];
+                ;
+            }
+        }
+    }
+}
