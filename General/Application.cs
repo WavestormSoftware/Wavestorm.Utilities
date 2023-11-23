1ae9e9cd2e5a2d0ae13683f7a2c5617d219844a7
Damian <contact@damians.space>
2023-11-23 19:00:20 +0100

    Add General/Application.cs to the repository with the specified code.

---
+++ General/Application.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace Wavestorm.Utilities;
+
+public abstract partial class Utilities
+{
+    public partial class General
+    {
+        public static class Application
+        {
+            /// <summary>
+            /// Gets the name of the application.
+            /// </summary>
+            /// <returns>The name of the application.</returns>
+            public static string GetApplicationName()
+            {
+                return AppDomain.CurrentDomain.FriendlyName;
+            }
+
+            /// <summary>
+            /// Gets the version of the application.
+            /// </summary>
+            /// <returns>The version of the application.</returns>
+            public static string GetApplicationVersion()
+            {
+                return Assembly.GetEntryAssembly()?.GetName().Version.ToString();
+            }
+        }
+    }
+}
