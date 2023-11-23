659d86efdda400c7f752bf8a255e452fdfebd7c7
Damian <contact@damians.space>
2023-11-23 19:03:31 +0100

    Add Storage.cs file with a GetTotalDiskSpace method for retrieving disk space.

---
+++ SystemInfo/Storage.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Wavestorm.Utilities;
+
+public abstract partial class Utilities
+{
+    public partial class SystemInfo
+    {
+        public static class Storage
+        {
+            /// <summary>
+            /// Get the total disk space of the specified drive. If driveName is not specified, get the total disk space of all drives combined.
+            /// </summary>
+            /// <param name="driveName">The name of the drive. If not specified, get the total disk space of all drives combined.</param>
+            /// <returns>The total disk space of the specified drive or all drives combined.</returns>
+            public static long GetTotalDiskSpace([Optional] string driveName)
+            {
+                if (string.IsNullOrEmpty(driveName))
+                {
+                    var totalSpace = DriveInfo.GetDrives()
+                        .Where(drive => drive.IsReady)
+                        .Sum(drive => drive.TotalSize);
+
+                    return totalSpace;
+                }
+                else
+                {
+                    DriveInfo drive = new DriveInfo(driveName);
+                    return drive.TotalSize;
+                }
+            }
+        }
+    }
+}
