b2247ce7746c204465a55db32c96b7a9d41fc3a9
Damian <contact@damians.space>
2023-11-23 19:03:18 +0100

    Add Serialization.cs file with utility methods for serializing and deserializing objects to/from JSON.

---
+++ General/Serialization.cs
@@ -0,0 +1,30 @@
+﻿namespace Wavestorm.Utilities;
+
+public abstract partial class Utilities
+{
+    public partial class General
+    {
+        public static class Serialization
+        {
+            /// <summary>
+            /// Serialize an object to a JSON string.
+            /// </summary>
+            /// <param name="obj">The object to serialize.</param>
+            /// <returns>A JSON string representation of the object.</returns>
+            public static string SerializeObject(object obj)
+            {
+                return System.Text.Json.JsonSerializer.Serialize(obj);
+            }
+
+            /// <summary>
+            /// Deserialize a JSON string to an object.
+            /// </summary>
+            /// <param name="json">The JSON string to deserialize.</param>
+            /// <returns>An object representation of the JSON string.</returns>
+            public static object DeserializeObject(string json)
+            {
+                return System.Text.Json.JsonSerializer.Deserialize<object>(json);
+            }
+        }
+    }
+}
