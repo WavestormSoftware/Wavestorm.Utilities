dfc6ff65c5a613f9fee3ca7375d80de3a8b2b8b2
Damian <contact@damians.space>
2023-11-23 19:01:06 +0100

    Add Logging.Debug class to the Utilities namespace.

---
+++ Logging/Debug.cs
@@ -0,0 +1,12 @@
+﻿namespace Wavestorm.Utilities;
+
+public abstract partial class Utilities
+{
+    public partial class Logging
+    {
+        public static class Debug
+        {
+            // TODO: Implement.
+        }
+    }
+}
