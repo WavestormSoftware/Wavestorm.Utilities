dc7b24edacc727087c90cb3b1a328bedafc40387
Damian <contact@damians.space>
2023-11-23 19:03:44 +0100

    Add Time.cs file with utility methods for getting Unix timestamps and time in specific timezones.

---
+++ General/Time.cs
@@ -0,0 +1,38 @@
+﻿namespace Wavestorm.Utilities;
+
+public abstract partial class Utilities
+{
+    public partial class General
+    {
+        public static class Time
+        {
+            /// <summary>
+            /// Get the current Unix timestamp.
+            /// </summary>
+            /// <returns>The current Unix timestamp.</returns>
+            public static long GetUnixTimestamp()
+            {
+                return DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
+
+            /// <summary>
+            /// Get the current Unix timestamp in milliseconds.
+            /// </summary>
+            /// <returns>The current Unix timestamp in milliseconds.</returns>
+            public static long GetUnixTimestampMilliseconds()
+            {
+                return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            }
+
+            /// <summary>
+            /// Get the current time in a specified timezone.
+            /// </summary>
+            /// <param name="timeZoneId">The timezone to get the current time in.</param>
+            /// <returns>The current time in the specified timezone.</returns>
+            public static DateTime GetTimeInTimezone(string timeZoneId)
+            {
+                return TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.FindSystemTimeZoneById(timeZoneId));
+            }
+        }
+    }
+}
