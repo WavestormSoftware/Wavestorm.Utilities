14609f78a343735c354b0d8642d71387983cfab7
Damianttje <58008234+Damianttje@users.noreply.github.com>
2023-11-23 20:06:04 +0100

    Add new file "Twilight.cs" to the project and change the assembly name to "Wavestorm.Utilities".

---
+++ Forms/Twilight.cs
@@ -0,0 +1,12 @@
+﻿namespace Wavestorm.Utilities;
+
+public partial class Utilities
+{
+    public partial class Files
+    {
+        public class Twilight
+        {
+            // TODO: Implement Twilight (A Wavestorm Software Multi-Platform application framework)
+        }
+    }
+}
