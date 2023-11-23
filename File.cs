f55ad398d04dd05d602c4f35cccd2b990b5ac584
Damian <contact@damians.space>
2023-11-23 01:37:05 +0100

    Add compression and decompression methods using GZip and improve error handling for file operations.

--- File.cs
+++ File.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.IO.Compression;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 namespace Wavestorm.Utilities;
 
@@ -13,7 +15,14 @@ public partial class Utilities
         /// <returns>The contents of the file.</returns>
         public static string? ReadFile(string path)
         {
-            return System.IO.File.ReadAllText(path);
+            try
+            {
+                return System.IO.File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
 
         /// <summary>
@@ -23,7 +32,56 @@ public partial class Utilities
         /// <returns>The contents of the file.</returns>
         public static byte[] ReadBinaryFile(string path)
         {
-            return System.IO.File.ReadAllBytes(path);
+            try
+            {
+                return System.IO.File.ReadAllBytes(path);
+            }
+            catch (Exception e)
+            {
+                return Array.Empty<byte>();
+            }
+        }
+        
+        /// <summary>
+        /// Compress binary data using GZip.
+        /// </summary>
+        /// <param name="data">The data to compress.</param
+        /// <returns>The compressed data.</returns
+        public static byte[] Compress(byte[] data)
+        {
+            try
+            {
+                using var memoryStream = new MemoryStream();
+                using var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress);
+                gZipStream.Write(data, 0, data.Length);
+                gZipStream.Close();
+                return memoryStream.ToArray();
+            }
+            catch (Exception e)
+            {
+                return Array.Empty<byte>();
+            }
+        }
+        
+        /// <summary>
+        /// Decompress binary data using GZip.
+        /// </summary>
+        /// <param name="data">The data to decompress.</param
+        /// <returns>The decompressed data.</returns
+        public static byte[] Decompress(byte[] data)
+        {
+            try
+            {
+                using var memoryStream = new MemoryStream();
+                using var gZipStream = new GZipStream(memoryStream, CompressionMode.Decompress);
+                gZipStream.Write(data, 0, data.Length);
+                gZipStream.Close();
+                return memoryStream.ToArray();
+            }
+            catch (Exception e)
+            {
+                return Array.Empty<byte>();
+            }
         }
         
         /// <summary>
@@ -171,7 +229,14 @@ public partial class Utilities
         /// <returns>The size of the file in bytes.</returns>
         public static long GetFileSize(string path)
         {
-            return new System.IO.FileInfo(path).Length;
+            try
+            {
+                return new System.IO.FileInfo(path).Length;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
         }
 
         /// <summary>
@@ -181,7 +246,14 @@ public partial class Utilities
         /// <returns>The creation time of the file.</returns>
         public static DateTime GetFileCreationTime(string path)
         {
-            return new System.IO.FileInfo(path).CreationTime;
+            try
+            {
+                return new System.IO.FileInfo(path).CreationTime;
+            }
+            catch (Exception e)
+            {
+                return DateTime.MinValue;
+            }
         }
 
         /// <summary>
@@ -191,7 +263,14 @@ public partial class Utilities
         /// <returns>The last access time of the file.</returns>
         public static DateTime GetFileLastAccessTime(string path)
         {
-            return new System.IO.FileInfo(path).LastAccessTime;
+            try
+            {
+                return new System.IO.FileInfo(path).LastAccessTime;
+            }
+            catch (Exception e)
+            {
+                return DateTime.MinValue;
+            }
         }
 
         /// <summary>
@@ -201,7 +280,76 @@ public partial class Utilities
         /// <returns>The last write time of the file.</returns>
         public static DateTime GetFileLastWriteTime(string path)
         {
-            return new System.IO.FileInfo(path).LastWriteTime;
+            try
+            {
+                return new System.IO.FileInfo(path).LastWriteTime;
+            }
+            catch (Exception e)
+            {
+                return DateTime.MinValue;
+            }
+        }
+        
+        /// <summary>
+        /// Get the MD5 hash of a file from the specified path.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The MD5 hash of the file.</returns>
+        public static string GetFileMd5(string path)
+        {
+            try
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(path))
+                    {
+                        byte[] hashBytes = md5.ComputeHash(stream);
+                        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return "";
+            }
+        }
+        
+        /// <summary>
+        /// Zips a directory to the specified path.
+        /// </summary>
+        /// <param name="directory">The directory to zip.</param>
+        /// <param name="path">The path to the zip file.</param>
+        /// <returns>True if the directory was zipped successfully, false otherwise.</returns>
+        public static bool ZipDirectory(string directory, string path)
+        {
+            try
+            {
+                System.IO.Compression.ZipFile.CreateFromDirectory(directory, path);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        
+        /// <summary>
+        /// Extracts a zip file to the specified directory.
+        /// </summary>
+        /// <param name="zip">The zip file to extract.</param>
+        /// <param name="directory">The directory to extract the zip file to.</param>
+        /// <returns>True if the zip file was extracted successfully, false otherwise.</returns>
+        public static bool ExtractZip(string zip, string directory)
+        {
+            try
+            {
+                System.IO.Compression.ZipFile.ExtractToDirectory(zip, directory);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
     }
 }
