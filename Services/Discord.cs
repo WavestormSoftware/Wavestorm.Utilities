c6dc06ee05a147cc45b11ef9c30124171e08cb65
Damianttje <58008234+Damianttje@users.noreply.github.com>
2023-11-28 13:51:34 +0100

    Add method `SendDiscordMessage` to send a discord message using a bot token and the discord API.

--- Services/Discord.cs
+++ Services/Discord.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Specialized;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
 
 namespace Wavestorm.Utilities;
 
@@ -8,6 +11,9 @@ public abstract partial class Utilities
 {
     public partial class Services
     {
+        /// <summary>
+        /// Provides methods for interacting with Discord.
+        /// </summary>
         public static class Discord
         {
             /// <summary>
@@ -38,6 +44,34 @@ public abstract partial class Utilities
                     return true;
                 }
                 catch
+                {
+                    return false;
+                }
+            }
+            
+            /// <summary>
+            /// Send a discord message through a discord bot using a discord bot token and making use of the discord API. [UNTESTED]
+            /// </summary>
+            /// <param name="botToken">The discord bot token.</param>
+            /// <param name="channelId">The discord channel ID.</param>
+            /// <param name="content">The content of the message.</param>
+            /// <returns>True if the message was sent successfully, false otherwise.</returns>
+            public static async Task<bool> SendDiscordMessage(string botToken, string channelId, string content)
+            {
+                if (Network.Status.IsConnectedToInternet())
+                {
+                    var url = $"https://discord.com/api/v9/channels/{channelId}/messages";
+                    using var httpClient = new HttpClient();
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bot", botToken);
+                    var data = new Dictionary<string, string>
+                    {
+                        ["content"] = content
+                    };
+                    var json = JsonSerializer.Serialize(data);
+                    var response = await httpClient.PostAsync(url, new StringContent(json, Encoding.UTF8, "application/json"));
+                    return response.IsSuccessStatusCode;
+                }
+                else
                 {
                     return false;
                 }
